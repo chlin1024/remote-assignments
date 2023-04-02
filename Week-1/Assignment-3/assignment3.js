2ca5bab9333cec14f1bec239f4c860bd8a8a3005
chlin1024 <40210128+chlin1024@users.noreply.github.com>
2023-04-02 12:19:38 +0800

    Add files via upload

---
+++ Week-1/Assignment-3/assignment3.js
@@ -0,0 +1,33 @@
+javascript
+function countAandB(input){
+  let aCount = 0;
+  let bCount = 0;
+  
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === 'a') {
+      aCount++;
+    } 
+    else if (input[i] === 'b') {
+      bCount++;
+    }
+  }
+  console.log(`${aCount} 'a' letters and ${bCount} 'b' letters`)
+}
+
+function toNumber(input){
+  let numArr = [];
+  for (let i = 0; i < input.length; i++) {
+    let charCode = input[i].charCodeAt(0) - 96; 
+      if (charCode >= 1 && charCode <= 26) { 
+      numArr.push(charCode); 
+      }
+  }
+  console.log(numArr);
+}
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1));
+console.log(toNumber(input1));
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2));
+console.log(toNumber(input2));

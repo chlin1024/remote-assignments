f7ac6ac4f78b673a66a100be085a8f05704ad2d8
Phoebelin <c.phoebe.lin@gmail.com>
2023-04-17 10:38:17 +0800

    week-3

--- Week-2/Assignment-1.js
+++ Week-2/Assignment-1.js
@@ -3,8 +3,8 @@
 2. findPosition: find the first position of the target number inside an array of numbers.
 The position should be counted starting from 0, if you can't find the target, please return -1*/
 function max(numbers) {
-    let max = 0;
-    for (i = 0; i < numbers.length; i++) {
+    let max = numbers[0]; //edit delcare max as first in array to fix problem if input are all smaller than 0
+    for (i = 1; i < numbers.length; i++) { // since first value in array is already max start with 2nd value in array
         if (numbers[i] > max) {
           max = numbers[i] ;
         }
@@ -12,19 +12,17 @@ function max(numbers) {
   return max
 }
  function findPosition(numbers, target) {
-    let position = 0;
+    let position = -1; //edit initialize position to -1 to indicate position is not found
     for (i = 0; i < numbers.length; i++) {
         if (numbers[i] === target) {
             position = i;
             break;
-        } else {
-            position = -1;
-        }
+        } // got rid of the else condition since position -1 alreay indicates position not found
     }
   return position;
 }
 
-console.log(max([1, 2, 4, 5])); // should print 5
+console.log(max([-1, -12, -4, -5])); // should print 5
 console.log(max([5, 2, 7, 1, 6])); // should print 7
 console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
 console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
---
+++ Week-3/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cookieParser = require('cookie-parser');
+
+app.get('/', (req, res) => {
+    res.send('Hello, My server!');
+});
+// Assignment 2
+app.get('/data', (req, res) => {
+    let data = req.query.number;
+    if (!data) {
+      res.send('Lack of Paraments');
+    } else if (isNaN(data) || data < 0) { // edit if number is negative is illegal
+      res.send('Wrong Paraments');
+    } else {
+      let n = Number(data);
+      let sum = ((1 + n) * n ) / 2;
+      res.send(`${sum}`);
+    } 
+});
+// Assignment 3
+app.use(express.static('public'));
+
+// Assignment 4
+app.use(cookieParser());
+
+app.get('/myName', (req, res) => {
+  const name = req.cookies.name;
+  if (name) {
+    res.send(`${name}`);
+  } else {
+    res.send(`<form action="/trackName" method="get">
+    <label for="name">Enter your name:</label>
+    <input type="text" id="name" name="name">
+    <button type="submit">Submit</button>
+  </form>`);
+  }
+})
+
+app.get('/trackName', (req, res) => {
+  const name = req.query.name;
+  res.cookie('name', name);
+  res.redirect('/myName');
+})
+
+// Listening to client request
+app.listen(3000, () => {
+    console.log(`The application is runnning on Localhost:${port}`);
+    console.log(`Listening to request on ${port}`);
+});

f2dc4d1ae808925f3405ff01b8624faeddfb2b8e
Phoebelin <c.phoebe.lin@gmail.com>
2023-04-23 23:46:57 +0800

    week 4 updates

--- Week-4/Assignment-3/database.js
+++ Week-4/Assignment-3/database.js
@@ -1,22 +1,72 @@
 //import mysql from 'mysql2';
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+
+/*const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'sef97jil',
     database: 'assignment'
-}).promise()
+}).promis
+
+
+
+const re = pool.query("SELECT * FROM user");
+console.log(re);
 
 module.exports = pool;
-
 async function getAssignment(){
-    const result = await pool.query("SELECT * FROM assignment");
+    const result = await pool.query("SELECT * FROM user");
     const rows = result[0];
     return rows;
 }
+*/
+/*async function getUserData() {
+    const pool = mysql.createPool({
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'sef97jil',
+      database: 'assignment'
+    }).promise()
+  
+    const [rows] = await pool.query("SELECT * FROM user");
+    return rows ;
 
-//aysnc function checkEmail(){
-    
-//}
+    async function getUser(id) {
+        const [rows] = await pool.query(`
+        SELECT * FROM user
+        WHERE email = ${id}
+        `)
+        console.log(rows);
+    }
+}
+
+async function logUsers() {
+        const users = await getUserData();
+        console.log(users);
+}
+  
+  logUsers();
+  getUser(1);
+*/
+// database.js
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'sef97jil',
+  database: 'assignment'
+});
+
+async function getUserData() {
+  const [rows] = await pool.query("SELECT * FROM user");
+  console.log(rows);
+  return rows;
+}
+
+module.exports = {
+  pool: pool,
+  getUserData: getUserData
+};
 

e2460d79c86fbd802ec5657921d4a9b4e71020f6
Phoebelin <c.phoebe.lin@gmail.com>
2023-04-25 12:54:10 +0800

    update backup.sql

--- Week-4/Assignment-3/backup.sql
+++ Week-4/Assignment-3/backup.sql
@@ -27,7 +27,7 @@ CREATE TABLE `user` (
   `email` varchar(225) NOT NULL,
   `password` varchar(225) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'kate8988@gmail.com','password1'),(2,'thisiscool@gmail.com','password2');
+INSERT INTO `user` VALUES (1,'kate8988@gmail.com','password1'),(2,'thisiscool@gmail.com','password2'),(3,'phoebe97p@gmail.com','88888');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-23 23:44:43
+-- Dump completed on 2023-04-25 12:52:38

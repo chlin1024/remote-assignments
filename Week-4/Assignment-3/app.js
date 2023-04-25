a871e05a6cd8dea40919b02425a0ccdf3c9dc0ad
Phoebelin <c.phoebe.lin@gmail.com>
2023-04-25 15:12:09 +0800

    update env variables settings as3 and gitignore

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -3,12 +3,14 @@ const app = express();
 const port = 3000;
 const pug = require('pug');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 const mysql = require('mysql2');
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'sef97jil',
-    database: 'assignment'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
 //const mysql = require('mysql2');
 //const pool = require('./database.js');
@@ -21,32 +23,32 @@ app.use(bodyParser.urlencoded({ extended: false }));//urlencode parser
 app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
-    res.send('Hello World!This is Week-4!')
+  res.send('Hello World!This is Week-4!')
 });
 app.get('/home_page', (req, res) =>{
-    res.render('home_page');
+  res.render('home_page');
 })
 app.get('/member', (req, res) =>{
-    res.render('member');
+  res.render('member');
 })
 
 app.post('/sign-in', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    //console.dir(email);
-    //console.dir(password);
-    if (!email || !password) {
-        res.render('home_page',{error: 'Wrong: email/password empty! try again.'});
-        return;
+  const email = req.body.email;
+  const password = req.body.password;
+  //console.dir(email);
+  //console.dir(password);
+  if (!email || !password) {
+    res.render('home_page',{error: 'Wrong: email/password empty! try again.'});
+    return;
     }
     const query = 'SELECT * FROM user WHERE email = ? AND password = ?';
     pool.query(
-        query,[email, password],
-        function(err, result) {
-            console.log(result);
-            if (result.length === 1) {
-                res.render('member');
-                return;
+      query,[email, password],
+      function(err, result) {
+        console.log(result);
+          if (result.length === 1) {
+            res.render('member');
+            return;
             } else {
                 res.render('home_page',{error: 'Wrong: try again.'});
                 return;
@@ -60,40 +62,38 @@ app.post('/sign-in', async (req, res) => {
 });
 
 app.post('/sign-up', async (req, res) =>{
-    const email = req.body['signup-email'];
-    const password = req.body['signup-password'];
-    //console.dir(email);
-    //console.dir(password);
+  const email = req.body['signup-email'];
+  const password = req.body['signup-password'];
+  //console.dir(email);
+  //console.dir(password);
 
-    if (!email || !password) {
-        res.render('home_page',{error: 'Wrong: email/password empty! try again.'});
-        return;
+  if (!email || !password) {
+    res.render('home_page',{error: 'Wrong: email/password empty! try again.'});
+    return;
     }
-    const query = 'SELECT * FROM user WHERE email = ?';
-    pool.query(
-        query,[email],
-        function(err, result) {
-            console.log(result);
-            if (result.length === 1) {
-                res.render('home_page',{error: 'You are existing member. Please sign-in'});
-                return;
-            } else if (result.length === 0) {
-                const insertQuery = 'INSERT INTO user (email, password) VALUES (?, ?)';
-                pool.query(insertQuery, [email, password],
-                    function(err, result){
-                        if (err) {
-                            res.render('home_page',{error: 'Wrong! try again.'});
-                            return; 
-                        } else {
-                            res.render('member');
-                        }
-                    })
+  const query = 'SELECT * FROM user WHERE email = ?';
+  pool.query(query,[email],
+    function(err, result) {
+      if (result.length === 1) {
+        res.render('home_page',{error: 'You are existing member. Please sign-in'});
+        return;
+      } else if (result.length === 0) {
+        const insertQuery = 'INSERT INTO user (email, password) VALUES (?, ?)';
+        pool.query(insertQuery, [email, password],
+          function(err, result){
+            if (err) {
+              res.render('home_page',{error: 'Wrong! try again.'});
+              return; 
             } else {
-                res.render('home',{error: 'Wrong! try again.'});
-                return;
+              res.render('member');
             }
-        }
-    );
+          })
+      } else {
+        res.render('home',{error: 'Wrong! try again.'});
+        return; 
+      }
+		}
+  );
 });
 
 app.listen(port,() =>{
--- Week-4/Assignment-3/database.js
+++ Week-4/Assignment-3/database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'sef97jil',
-    database: 'assignment'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
+
 //const query = ''
 pool.query(
   'SELECT * FROM user WHERE email = ?',
